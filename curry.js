95c8cd09a776c8933f756874ea9c81501d2da2c3
Theodore Robert Cackowski <theodore.cackowski@gmail.com>
2015-02-22 16:46:22 -0600

    fixed some logic with the module loader

--- curry.js
+++ curry.js
@@ -1,13 +1,6 @@
 
 var
 
-I = function(x){ return x; },
-K = function(x){
-  return function(){
-    return x;
-  };
-},
-undefined = I(),
 applyStrict = function(fn,context,argumentArray){
   return typeof(fn) === 'function'
   ?  fn.apply(context,argumentArray)
@@ -58,15 +51,5 @@ curry = function(arity,fn){
       );
   });
 },
-apply = curry(function(fn,context,args){
-  return applyStrict(fn,context,args);
-}),
-curry2 = curry(2),
-curry3 = curry(3),
-create = curry3(function(p,o){
-  var B = function(){};
-  B.prototype = p;
-  return extend(new B(),o);
-}),
 
 z;
--- define.js
+++ define.js
@@ -3,25 +3,72 @@ var
 sourceMap = {
   'util':'util.js'
 },
-modules ={},
-loadModule = function(name){
-  var head = document.head
-    ||document.getElementsByTagName('head')[0];
-  var script = document.createElement('script');
-  script.src = sourceMap[src];
+modules = {},
+scripts = {},
+loadScript = function(url){
+  var
+  promise = Promise(),
+  script = document.createElement('script'),
+  head = document.getElementsByTagName('head')[0],
+  afterLoad = function(){
+    if(
+        !this.readyState
+      || this.readyState === 'loaded'
+      || this.readyState === 'complete'
+    ){
+      script.onload
+        = script.onreadystatechange
+        = null;
+      promise.resolve();
+    }
+  };
+  script.src
+    = url;
+  script.onload
+    = script.onreadystatechange
+    = afterLoad;
   head.appendChild(script);
+  return promise;
 },
-getPromise = function(name){
+getModulePromise = function(name){
   return modules[name]
-    =  modules[name]
-    || loadModule(name);
+    =    modules[name]
+    || (
+      //Side-effect :-\
+      getScriptPromise(sourceMap(name))
+      && Promise()
+    );
 },
+getScriptPromise = function(url){
+  return scripts[url]
+    =    scripts[url]
+    ||   loadScript(url);
+},
+
 define = function(name,reqs,fn){
   //needs overloading
-  return modules[name] = chain(
-    K(reqs),
-    map(dot(modules)),
-    Promise.s.all,
-    dotApply('then',[fn])
-  )();
-};
+  var modulePromises = [];
+  for (var i in reqs) {
+    var
+    req = reqs[i],
+    modulePromise = getModulePromise(req);
+    modulePromises.push(modulePromise);
+  }
+  var newPromise = Promise.s
+    .all(modulePromises)
+    .then(fn);
+  // var
+  // newPromise = chain(
+  //   K(reqs),
+  //   map(getModulePromise),
+  //   Promise.s.all,
+  //   dotApply('then',[fn])
+  // )(),
+  oldPromise = modules[name];
+  newPromise.then(function(val){
+    oldPromise.resolve(val);
+  });
+  return modules[name] = newPromise;
+},
+
+z;
