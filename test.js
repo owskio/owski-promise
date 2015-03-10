d065d73feacb5395870fdb89bcc21c5d8e06a3cb
Theodore Robert Cackowski <theodore.cackowski@gmail.com>
2015-03-09 19:46:54 -0500

    more module syntax

--- test.js
+++ test.js
@@ -1,9 +1,16 @@
 
 require('must');
-//var util = require('util');
+var Promise = require('./promise');
 
 describe('Promises',function(){
   it('should publish only on resolution',function(){
-    (5).should.equal(5);
+    (5).must.equal(5);
+  });
+  it('should immediately resolve when constructed with values',function(done){
+    var p = Promise(5);
+    p.then(function(five){
+      five.must.equal(5);
+      done();
+    });
   });
 });
