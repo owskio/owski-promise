9be926051d49a64e40698006386e6b424e1ce5ce
Theodore Robert Cackowski <theodore.cackowski@gmail.com>
2015-03-09 20:26:29 -0500

    fixing bugs and writing tests!!!

---
+++ curry.mocha.js
@@ -0,0 +1,65 @@
+
+require('must');
+
+var
+haskell = require('./curry'),
+curry = haskell.curry,
+partial = haskell.partial,
+applyStrict = haskell.applyStrict,
+arrayFunction = haskell.arrayFunction,
+getArity = haskell.getArity,
+
+product = function(a,b,c){
+  return a*b*c;
+};
+
+describe('applyStrict',function(){
+  it('should work on ordinary functions',function(){
+    applyStrict(product,null,[2,3,4])
+      .must
+      .equal(
+        product(2,3,4)
+      );
+  });
+});
+describe('argumentsToArray',function(){
+  it('should call callbacks with argument arrays',function(){
+    arrayFunction(function(args){
+      args.must.eql([4,5,6]);
+    })(4,5,6);
+  });
+});
+describe('getArity',function(){
+  it('should get an array of expected args',function(){
+    getArity(function(x,y,z){
+      /* Dont care */
+    })
+    .must.eql(
+      ['x','y','z']
+    );
+  });
+});
+
+
+describe('curry',function(){
+
+  var f = curry(function(a,b,c){
+    return a + b + c;
+  });
+
+  it('should generalize application',function(){
+    f('a','b','c')
+      .must
+      .equal(
+        f('a')('b')('c')
+      );
+  });
+
+  it('should generalize partial application',function(){
+    partial(f,['a','b'])()
+      .must
+      .equal(
+        f('a')('b')()
+      );
+  });
+});
