9a66f2ec017f35b803639a90b5ce7cf5a6880664
Theodore Robert Cackowski <theodore.cackowski@gmail.com>
2015-03-09 20:39:58 -0500

    saving

--- curry.js
+++ curry.js
@@ -1,6 +1,6 @@
 
 var
- 
+
 applyStrict = function(fn,context,argumentArray){
   return typeof(fn) === 'function'
   ?  fn.apply(context,argumentArray)
@@ -8,7 +8,7 @@ applyStrict = function(fn,context,argumentArray){
 },
 argumentsToArray = function(argumentsObject){
   var theSlice = Array.prototype.slice;
-  return applyStrict(slice,argumentsObject,[0]);
+  return applyStrict(theSlice,argumentsObject,[0]);
 },
 arrayFunction = function(fn){
   return function(){
@@ -22,25 +22,27 @@ getArity = function(fn){
     .split('{')[0]
     .replace(' ','')
     .match(/[^,\(\)\s]+/g);
-  args.unshift();
+  args.shift();
   return args;
 },
 partial = function(fn,argumentArray){
   return arrayFunction(function(args){
-    return applyStrict(fn,this,argumentArray.concat(args));
+    return applyStrict(
+      fn,this,argumentArray.concat(args)
+    );
   });
 },
 curry = function(arity,fn){
   if(typeof(fn) === 'undefined'){
     if(typeof(arity) === 'function'){
-      arity = getArity(fn);
+      fn = arity;
+      arity = getArity(fn).length;
     } else if(typeof(arity) === 'number'){
       return function(fn){
         return curry(arity,fn);
       };
     }
   }
-
   return arrayFunction(function (args) {
     var len = args.length;
     return len >= arity || !len
@@ -50,6 +52,13 @@ curry = function(arity,fn){
         partial(fn,args)
       );
   });
-},
+};
 
-z;
+module.exports = {
+  applyStrict: applyStrict,
+  argumentsToArray: argumentsToArray,
+  arrayFunction: arrayFunction,
+  getArity: getArity,
+  partial: partial,
+  curry: curry
+};
