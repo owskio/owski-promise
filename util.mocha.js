826f3fa1d4e359a3c67923c52f18e579619bbcfd
Theodore Robert Cackowski <theodore.cackowski@gmail.com>
2015-03-16 20:18:41 -0500

    more tests

--- util.mocha.js
+++ util.mocha.js
@@ -97,4 +97,13 @@ describe('Util',function(){
     created.must.have.property('a',1);
     created.must.have.property('b',2);
   });
+  it('createLazy must create distinct objects',function(){
+    var
+    c = createLazy({a:1},{b:2}),
+    created1 = c(),
+    created2 = c();
+
+    created1.must.not.equal(created2);
+    (created1 === created2).must.not.be.true();
+  });
 });
