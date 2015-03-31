16076d9cf1538592a232a5b6d699621a1e580e97
Theodore Robert Cackowski <theodore.cackowski@gmail.com>
2015-03-31 17:05:56 -0500

    bus stop

--- beacon.js
+++ beacon.js
@@ -9,12 +9,35 @@ publish = function(b,v){
     apply(fn,b,[v]);
   },b.observers);
 },
-Beacon = createLazy({
-  publish: antitype(publish)
-},{
-  observers: [],
-  value: undefined,
+Beaconify = create({
+  publish: antitype(publish),
+  set: function(v){
+    this.value = v;
+    this.publish(v);
+  },
+  observer: function(fn){
+    this.observers.push(fn);
+  },
+  sets: function(b){
+    return this.observer(function(v){
+      return b.set(v);
+    });
+  },
+  map: function(fn){
+    var b = Beacon();
+    this.observer(function(v){
+      return b.set(fn(v));
+    });
+    return b;
+  },
+  z:z
 }),
+Beacon = function(v){
+  return Beaconify({
+    observers: [],
+    value: v,
+  });
+},
 z;
 
 module.exports = Beacon;
