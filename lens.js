ec756f7e1a8b32022aab9468fc3a4cd6ca9ed500
Theodore Robert Cackowski <theodore.cackowski@gmail.com>
2015-04-11 08:37:32 -0500

    beginning in sun room

--- lens.js
+++ lens.js
@@ -7,6 +7,25 @@ require('./curry').mport(function(curry,curry2){
   acc = curry2(function(k,o,v){
     return o[k] = v || o[k];
   }),
+  lens = curry(function(how,next,o,prev) {
+    return next(prev(o),how);
+  }),
+  farm = lens(acc('farm')),
+  cow = lens(acc('cow')),
+  farmCow = compose(farm,cow)
+  farm = function(l,o){
+    return l(o)
+  },
+  fn(next,prev,o){
+    return next(acc('farm'),prev(o));
+  }
+
+
+  milk = lens(function(l,o){ return l(o).milk; }),
+  set = curry(function(val,o){
+    l(o)
+  }),
+
   compose(farm,cow,milk,set('eww'))
   ({farm:{cow:{milk:'mmm'}}})
   === {farm:{cow:{milk:'eww'}}}
@@ -14,30 +33,10 @@ require('./curry').mport(function(curry,curry2){
   lens = curry(function(fn,l,o){
     return fn((l||I),(o));
   }),
-  farm = lens(function(l,o){ return l(o).farm; }),
-  cow = lens(function(l,o){ return l(o).cow; }),
-  milk = lens(function(l,o){ return l(o).milk; }),
-  set = curry(function(val,o){
-    l(o)
-  }),
+
 a1 = {a:1}
 l(a)(function(v){ return v+3;})(a1)
 //{a:4}
-
-
-lens = curry(function(how,next,prev,o) {
-  return next(how,prev(o));
-}),
-
-
-  example = function(){
-
-  },
-  wheels = unit('wheels')
-
-  attr = curry(function(attrName,obj,fn){
-    return fn(obj[attrName]);
-  }),
 
   ;
 
